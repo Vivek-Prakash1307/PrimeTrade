e85b27bec498af48fe71c99702ccdcb57b250582
Vivek Prakash <alivevivek8@gmail.com>
2026-01-19 15:28:41 +0530

    adding the postman.collection.json file

---
+++ server/controllers/taskController.js
@@ -0,0 +1,81 @@
+const Task = require("../models/Task");
+
+// CREATE TASK
+exports.createTask = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+
+    if (!title) {
+      return res.status(400).json({ message: "Title is required" });
+    }
+
+    const task = await Task.create({
+      title,
+      description,
+      user: req.user._id
+    });
+
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// GET ALL TASKS (LOGGED IN USER)
+exports.getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({ user: req.user._id }).sort({
+      createdAt: -1
+    });
+
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// UPDATE TASK
+exports.updateTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    // Ensure task belongs to user
+    if (task.user.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+
+    task.title = req.body.title || task.title;
+    task.description = req.body.description || task.description;
+    task.status = req.body.status || task.status;
+
+    const updatedTask = await task.save();
+    res.json(updatedTask);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// DELETE TASK
+exports.deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    // Ensure task belongs to user
+    if (task.user.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+
+    await task.deleteOne();
+    res.json({ message: "Task removed" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
